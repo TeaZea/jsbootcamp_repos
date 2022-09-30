c80b12f68f7944e793c5523916b393bb7c635439
Stefano <sena.stefano@gmail.com>
2022-09-30 10:14:12 -0400

    Completed C1, L2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,6 @@
+//Lecture 1
+
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 -10);
 
@@ -20,14 +23,50 @@ let myFirstJob = "Programmer";
 let myCurrentJob = "Teacher";
 
 console.log(myFirstJob);
+*/
 
+// L1 Assignment
 
-// JS Fundamentals P1 Assignments
-
-// L1
 let country = "Canada";
 let continent = "North America";
-let population = 42;
+let population = 42; // In millions
 
-// L2
+
+
+// Lecture 2
+/*
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
+
+// L2 Assignment
+
+let isIsland = country;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+
+// Lecture 3
 
