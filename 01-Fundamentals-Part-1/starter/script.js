1cd1fac844702641ed464ea12df7f770d54b9c8d
Stefano <sena.stefano@gmail.com>
2022-10-03 11:02:06 -0400

    Completed L5, Basic Operators.'

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -63,7 +63,6 @@ let isIsland = country;
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
-console.log(typeof language);
 
 
 
@@ -83,8 +82,49 @@ job = "teacher";
 const language = "English";
 
 
-
 // Lecture 4
+//Math Operators
+/*
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
 
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = "Jonas";
+const lastName = "Schmedt";
+console.log(firstName + " " + lastName);
+
+// Assignment Opeorators
+let x  = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4;
+x++;
+x--;
+x--; // = 99
+console.log(x);
+
+//Comparison Operators
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018); // Math Operators first, then Comparison.
+*/
+
+// L4 Assignment
+
+let pop = population / 2;
+pop++;
+console.log(pop);
+
+const description = "Canada is in North America. The two official languages are French and English";
+
+
+
+//Lecture 5
 
 
