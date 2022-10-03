8c03e3a3810f0261446883a26eb4a4667dad8f6a
Stefano <sena.stefano@gmail.com>
2022-10-03 11:09:57 -0400

    Completed L5, Operator Precidence.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -125,6 +125,17 @@ const description = "Canada is in North America. The two official languages are 
 
 
 
-//Lecture 5
+//Lecture 5 - Operator Prededence
 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
 
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
