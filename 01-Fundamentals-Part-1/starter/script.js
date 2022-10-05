748faf94bbc523b1bd3816a5dcaafc1bc9bf5fe6
Stefano <sena.stefano@gmail.com>
2022-10-05 11:24:21 -0400

    Truthy FAlsy Lecture

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -227,3 +227,26 @@ console.log("23" * "2"); // '*' and  '/' converts everything to numbers only
 
 // Truthy and Falsy
 
+//5 falsy values: 0, '', undefined, null, NaN. WILL become false hen converted to a Boolean
+/*
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if(money) {
+    console.log("Don't spend it all ;)");
+} else {
+    console.log("You should get a job!");
+}
+
+let height;
+if(height) {
+    console.log("YAY! height is defined!");
+} else {
+    console.log("Height is UNDEFINED!");
+}
+*/
+
