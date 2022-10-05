0784fd01ace1b567bfe2afe6e46293edeb5f26e1
Stefano <sena.stefano@gmail.com>
2022-10-05 10:03:34 -0400

    Completed if/else lecture and assignment.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -172,4 +172,37 @@ console.log(descriptionEdit)
 
 
 
-//Lecture 7 if/else Statements
+// if/else Statements
+/*
+const age = 15;
+
+if(age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft}`);
+}
+
+const birthYear = 1991;
+let century;
+
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
+
+// if/else Assignment
+if(population > 33) {
+    console.log(`${country} is above the average`)
+} else {
+    const popDiff = 33 - population;
+    console.log(`${country} is ${popDiff} million below the average`)
+}
+
+
+
+// Type Conversion and Coercion
+
