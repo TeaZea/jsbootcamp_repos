3fe9fcd264160446cb19e5e9677ef3aa9d607b0e
Stefano <sena.stefano@gmail.com>
2022-10-06 09:52:28 -0400

    Completed Logical Operator challenges + bonus

--- 01-Fundamentals-Part-1/starter/challenge1.js
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -22,3 +22,34 @@ if(markHigherBMI) {
 } else {
     console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi})!`)
 }
+
+
+// Challenge 3
+
+/* Data 1
+const avgDolphines = (96 + 108 + 89) / 3;
+const avgKoalas = (88 + 91 + 110) / 3;
+
+if (avgDolphines > avgKoalas) {
+    console.log("Dolphines win!");
+} else if (avgDolphines < avgKoalas) {
+    console.log("Koalas win!");
+} else {
+    console.log("It's a tie!");
+}
+*/
+
+// Challenge 3: Bonus
+
+const avgDolphines = (97 + 112 + 80) / 3;
+const avgKoalas = (109 + 95 + 50) / 3;
+
+if (avgDolphines > avgKoalas && avgDolphines >= 100) {
+    console.log("Dolphines win!");
+} else if (avgDolphines < avgKoalas && avgKoalas >= 100) {
+    console.log("Koalas win!");
+} else if (avgDolphines === avgKoalas && avgKoalas >= 100 && avgDolphines >= 100){
+    console.log("It's a tie!");
+} else {
+    console.log("No one wins!");
+}
