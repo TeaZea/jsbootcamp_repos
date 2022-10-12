4f9a7781c8cc7f9358f4bc097210c8bffd540dda
Stefano <sena.stefano@gmail.com>
2022-10-12 09:53:11 -0400

    Learned abouit strict mode and debugging

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,13 @@
+// Strict Mode
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I cant drive!');
+
+// Example reserved words/variables by strict mode
+// const interface = "Audio";
+// const private = 534;
+
