18c6182c3bc7b54a4fe5ac0b259a8436b1c1a8cd
Stefano <sena.stefano@gmail.com>
2022-10-12 11:15:08 -0400

    Completed Functions lecture and assignments

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,7 @@
-// Strict Mode
+// Strict Mode + S.M Lecture
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -8,6 +9,49 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I cant drive!');
 
 // Example reserved words/variables by strict mode
-// const interface = "Audio";
-// const private = 534;
+const interface = "Audio";
+const private = 534;
+*/
+
+
+
+// Functions Lecture
+/*
+function logger() {
+    console.log("My name is Jonas");
+}
+
+// Calling,running, invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+// Functions Assignment
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and it's capital city is ${capitalCity}.`;
+}
+
+const canada = describeCountry('Canada', 42, 'Ottawa');
+const usa = describeCountry('USA', 300, 'Washington DC');
+const bulgaria = describeCountry('Bulgaria', 6, 'Sofia');
+console.log(canada, usa, bulgaria);
+
+
+
+// Declerations and Expressions
 
