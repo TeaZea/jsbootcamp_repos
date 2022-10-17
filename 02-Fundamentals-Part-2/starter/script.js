d4175d9917ae21df4138b02db476b825641fcc9c
Stefano <sena.stefano@gmail.com>
2022-10-17 10:26:22 -0400

    Calling other functions within functions lecture and assignment completed.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -123,6 +123,32 @@ console.log(perc4);
 
 
 // Functions Calling Other Functions
+/*
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
+*/
+
+//Functions Calling Functions Assignment
+
+function describePopulation(country, population) {
+    const pop = percentageOfWorld1(population);
+    return `${country} has ${population} million people, which is about ${pop}% of the world.`;
+}
+
+console.log(describePopulation('Canada', 42));
 
 
 
+//
