5bff4329f98bf812daa74084919ba426b28f4745
Stefano <sena.stefano@gmail.com>
2022-10-18 09:09:06 -0400

    Completed Array lecture and assignments

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -177,3 +177,60 @@ console.log(yearsUntilRetirement(1950, 'Mike'));
 */
 
  
+
+// Intro to Arrays
+/*
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Micheal", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991,1984,2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
+
+// Array Assignment
+
+const populations = [300, 42, 150, 6];
+
+if (populations.length === 4) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+const percentages = [percentageOfWorld1(42), percentageOfWorld1(300), percentageOfWorld1(150), percentageOfWorld1(6)]
+console.log(percentages);
+
+
+
+// Methods
+
