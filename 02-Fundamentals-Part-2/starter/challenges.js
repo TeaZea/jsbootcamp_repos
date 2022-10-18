7b601bb120d70d46bef636724707c2e9bd2b80f2
Stefano <sena.stefano@gmail.com>
2022-10-18 10:15:10 -0400

    Completed coding challenge 2

--- 02-Fundamentals-Part-2/starter/challenges.js
+++ 02-Fundamentals-Part-2/starter/challenges.js
@@ -17,3 +17,21 @@ const checkWinner = function(avgDolphins, avgKoalas) {
 }
 
 checkWinner(scoreDolphins, scoreKoalas);
+
+
+
+// Challenge 2
+
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? 15 / 100 * bill : 20 / 100 * bill;
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 55, 44];
+console.log(calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]));
+
+const total = [bills[0] + calcTip(bills[0]), bills[1] + calcTip(bills[1]), bills[2] + calcTip(bills[2])];
+console.log(total);
+
+
