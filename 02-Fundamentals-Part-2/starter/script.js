41b3c6a79eaa0645b7f4fc523d339f08afbff927
Stefano <sena.stefano@gmail.com>
2022-11-02 10:45:45 -0400

    Completed While Loop lecture

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -547,4 +547,18 @@ for (let i = 0; i < listOfNeighbours.length; i++) {
 
 
 // While Loop
+/*
+let rep = 1;
+while (rep <= 10 ){
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;
+}
 
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log('Loop is about to end...');
+}
+*/
