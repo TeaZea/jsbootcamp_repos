2708bab68d00c4f3b42122ee81971ec0ffa8627f
Stefano <sena.stefano@gmail.com>
2022-11-07 16:00:27 -0500

    Created variables for the queried selections to use, craeted functions to open and close modals, assigned those functions to event clicks to make the site functional.

---
+++ 06-Modal/starter/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// assign variables to items to use
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Function to open modals
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// Function to close modals
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
