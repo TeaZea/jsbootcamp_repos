57c44f559de1108dc20eb87c45f19f1282ae7c5e
Stefano <sena.stefano@gmail.com>
2022-11-07 13:55:45 -0500

    Refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -42,24 +42,28 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     // Too high guess
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High!';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too High!' : 'Too Low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'YOU LOST!';
-    }
-    //Too low guess
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'YOU LOST!';
+      document.querySelector('.score').textContent = 0;
     }
   }
+
+  //     //Too low guess
+  //   } else if (guess < secretNumber) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = 'Too Low!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = 'YOU LOST!';
+  //     }
+  //   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
