e1d144e2616391389294048823af04f4d62ff838
Stefano <sena.stefano@gmail.com>
2022-11-07 10:25:53 -0500

    DOM manupulation exercises

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// // document.querySelector('.message').textContent = 'Correct Number!';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// // Click Events
+// // Select class, add event listener (function expression)
+// document.querySelector('.check').addEventListener('click', function () {
+//   //ensure guess variable is a number
+//   const guess = Number(document.querySelector('.guess').value);
+//   console.log(guess, typeof guess);
+
+//   //DOM manupulation if no guess
+//   if (!guess) {
+//     document.querySelector('.message').textContent = 'No Number!';
+//   }
+// });
